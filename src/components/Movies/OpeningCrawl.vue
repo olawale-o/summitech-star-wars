fb9cdbdc7b2962fd642e11f0d7c892f585f8b6fa
olawale-o <omoogunolawale@gmail.com>
2023-02-02 19:03:41 +0100

    Create OpeningCrawl component

---
+++ src/components/Movies/OpeningCrawl.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ openingCrawl }}</p>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'OpeningCrawl',
+  props: {
+    openingCrawl: {
+      type: String,
+    },
+  },
+  setup() {
+    return {}
+  },
+});
+</script>
+
+<style scoped>
+  p {
+    font-size: 1.5rem;
+  }
+</style>
