4478e7d2d8f6ff9611fdba0e4eb19f5698c7fede
olawale-o <omoogunolawale@gmail.com>
2023-02-02 19:17:34 +0100

    Animate movie opening crawl

--- src/components/Movies/OpeningCrawl.vue
+++ src/components/Movies/OpeningCrawl.vue
@@ -1,5 +1,9 @@
 <template>
-  <p>{{ openingCrawl }}</p>
+  <div class="marquee">
+    <p>
+      {{ openingCrawl  }}
+    </p>
+  </div>
 </template>
 
 <script>
@@ -18,7 +22,49 @@ export default defineComponent({
 </script>
 
 <style scoped>
-  p {
-    font-size: 1.5rem;
+  .marquee {
+    width: 100%;
+    margin-block: 20px;
+    height: 50px;	
+    overflow: hidden;
+    position: relative;
+    background: yellow;
+    color: orange;
+    border: 1px solid orange;
   }
+  .marquee p {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    line-height: 50px;
+    text-align: center;
+    color: #000;
+    -moz-transform: translateX(100%);
+    -webkit-transform: translateX(100%);	
+    transform: translateX(100%);
+    -moz-animation: marquee 15s linear infinite;
+    -webkit-animation: marquee 15s linear infinite;
+    animation: marquee 15s linear infinite;
+  }
+  @-moz-keyframes marquee {
+    0%   { -moz-transform: translateX(100%); }
+    100% { -moz-transform: translateX(-100%); }
+  }
+  @-webkit-keyframes marquee {
+    0%   { -webkit-transform: translateX(100%); }
+    100% { -webkit-transform: translateX(-100%); }
+  }
+  @keyframes marquee {
+    0%   { 
+    -moz-transform: translateX(100%);
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%); 		
+  }
+  100% { 
+    -moz-transform: translateX(-100%);
+    -webkit-transform: translateX(-100%);
+    transform: translateX(-100%); 
+  }
+}
 </style>
