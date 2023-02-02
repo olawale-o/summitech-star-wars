e96e3e6ff8b0962f182758e5c71a99795e91724f
olawale-o <omoogunolawale@gmail.com>
2023-02-02 19:02:58 +0100

    Fetch movie characters

--- src/views/Movies/index.vue
+++ src/views/Movies/index.vue
@@ -6,39 +6,75 @@
     >
       Choose a star wars movies
     </button>
-    <MovieList :movies="movies" />
+    <template v-if="movies.length > 0">
+      <MovieList :movies="movies" @movie-selected="movieSelected" />
+    </template>
   </div>
+  <OpeningCrawl v-if="movie.movie.opening_crawl" :openingCrawl="movie.movie.opening_crawl"/>
+  <!-- <template v-if="movie.movie.characters"> -->
+  <CharacterTable :characters="characters" />
+  <!-- </template> -->
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from 'vue';
+import { onMounted, ref, reactive } from 'vue';
 import MovieList from '@/components/Movies/MovieList.vue';
+import OpeningCrawl from '@/components/Movies/OpeningCrawl.vue';
+import CharacterTable from '@/components/Movies/CharacterTable.vue';
 
-export default defineComponent({
+export default {
   name: 'MoviesView',
   components: {
     MovieList,
+    OpeningCrawl,
+    CharacterTable,
   },
   setup() {
     const movies = ref([]);
-    onMounted(() => {
-      fetch('https://swapi.dev/api/films/')
-        .then((response) => response.json())
+    const movie = reactive({
+      movie: {},
+    });
+    const characters = ref([]);
+    const fetchCharacters = async (url) => {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    };
+    const movieSelected = async (url) => {
+      console.log(url);
+      const response = await fetch(url);
+      const data = await response.json();
+      // console.log(data);
+      movie.movie = data;
+      Promise.all(data.characters.map((url) => fetchCharacters(url)))
         .then((data) => {
           console.log(data);
-          const sortedData = data.results.sort((a, b) => {
-            return new Date(a.release_date) - new Date(b.release_date);
-          });
-          movies.value = sortedData;
+          characters.value = data;
         }).catch((error) => {
           console.log(error);
-        })
+        });
+    };
+    onMounted(() => {
+    //   fetch('https://swapi.dev/api/films/')
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       console.log(data);
+    //       const sortedData = data.results.sort((a, b) => {
+    //         return new Date(a.release_date) - new Date(b.release_date);
+    //       });
+    //       movies.value = sortedData;
+    //     }).catch((error) => {
+    //       console.log(error);
+    //     })
     });
     return {
-      movies
+      movies,
+      movie,
+      movieSelected,
+      characters
     } 
   },
-});
+};
 </script>
 
 <style scoped>
