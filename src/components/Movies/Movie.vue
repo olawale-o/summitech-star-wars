d334338d8da4fe834c9e1e73bab080906e8b19a5
olawale-o <omoogunolawale@gmail.com>
2023-02-02 15:48:32 +0100

    Render movie item

---
+++ src/components/Movies/Movie.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="movie">
+    <button
+      type="button"
+      class="movie-button"
+    >
+      {{ movie.title }}
+    </button>
+  </li>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'MovieItem',
+  props: {
+    movie: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {}
+  },
+});
+</script>
+
+<style scoped>
+  .movie-button {
+    padding: 0.8rem;
+    width: 100%;
+    font-size: 1.5rem;
+  }
+</style>
---
+++ src/components/Movies/MovieList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="movielist-container">
+    <ul class="movielist">
+      <MovieItem v-for="(movie, i) in movies" :key="i" :movie="movie" />
+    </ul>
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+  import MovieItem from '@/components/Movies/Movie.vue';
+  export default defineComponent({
+    name: 'MovieList',
+    components: {
+      MovieItem,
+    },
+    props: {
+      movies: {
+        type: Array,
+        required: true,
+      },
+    },
+    setup() {
+    
+    return {}   
+    
+    },
+  });
+</script>
+
+<style scoped>
+.movielist-container {
+  width: 100%;
+}
+.movielist {
+  width: 100%;
+  max-height: 200px;
+  overflow-y: scroll;
+}
+</style>
