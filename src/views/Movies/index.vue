2eadb82f11f6a67cd444ea92b904eb3feeb810d9
olawale-o <omoogunolawale@gmail.com>
2023-02-02 15:48:07 +0100

    Fetch movies and display on dropdwon

---
+++ src/views/Movies/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="movies-dropdown">
+    <button
+      type="button"
+      class="dropdown-toggle"
+    >
+      Choose a star wars movies
+    </button>
+    <MovieList :movies="movies" />
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from 'vue';
+import MovieList from '@/components/Movies/MovieList.vue';
+
+export default defineComponent({
+  name: 'MoviesView',
+  components: {
+    MovieList,
+  },
+  setup() {
+    const movies = ref([]);
+    onMounted(() => {
+      fetch('https://swapi.dev/api/films/')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          movies.value = data.results;
+        }).catch((error) => {
+          console.log(error);
+        })
+    });
+    return {
+      movies
+    } 
+  },
+});
+</script>
+
+<style scoped>
+.movies-dropdown {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.dropdown-toggle {
+  display: block;
+  background: yellow;
+  padding: 1rem;
+  font-size: 1.5rem;
+  outline: none;
+  border: none;
+}
+</style>
