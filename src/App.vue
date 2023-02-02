e0be9749487aec2ceca299ba2f0b7747b457e4cc
olawale-o <omoogunolawale@gmail.com>
2023-02-02 15:47:33 +0100

    Render movie list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-content">
+    <h1 class="app-title">
+      Star Wars
+    </h1>
+    <MoviesView />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MoviesView from '@/views/Movies/index.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MoviesView
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+}
+.app-content {
+  max-width: 760px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.app-title {
+  font-size: 8rem;
 }
 </style>
