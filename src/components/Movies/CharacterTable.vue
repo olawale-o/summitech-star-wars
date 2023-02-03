98cc201a94764e94330bf9e60e3026439457d286
olawale-o <omoogunolawale@gmail.com>
2023-02-03 09:22:05 +0100

    Sort list by name and height

--- src/components/Movies/CharacterTable.vue
+++ src/components/Movies/CharacterTable.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="character-table">
+    <div class="gender">
+      <select v-model="gender">
+        <option value="all">All</option>
+        <option value="m">m</option>
+        <option value="f">f</option>
+        <option value="other">other</option>
+      </select>
+    </div>
    <div class="header">
-     <button type="button" class="filter-btn">Name</button>
-     <button type="button" class="filter-btn">Gender</button>
-     <button type="button" class="filter-btn">Height</button>
+     <button type="button" class="filter-btn" @click="filterByName('name')">
+      Name
+    </button>
+     <button type="button" class="filter-btn">
+      Gender
+    </button>
+     <button type="button" class="filter-btn" @click="filterByHeight('height')">
+      Height
+    </button>
    </div>
    <div class="cells">
-     <CharacterRow v-for="(character, i) in characters" :key="i" :character="character" />
+     <CharacterRow v-for="(character, i) in filterPersons" :key="i" :character="character" />
    </div>
   </div>
 </template>
 
 <script>
+  import { ref, watch } from 'vue';
   import CharacterRow from '@/components/Movies/CharacterRow.vue';
   export default {
     name: 'CharacterTable',
@@ -23,8 +38,83 @@
         type: Array,
       },
     },
-    setup() {
-      return {}
+    setup(props) {
+      const persons = ref(props.characters);
+      const filterPersons = ref(props.characters);
+      const sortOrder = ref(true);
+      const gender = ref('all');
+      const filterByName = (key = '') => {
+        if (!sortOrder.value) {
+          filterPersons.value = filterPersons.value.sort((a, b) => {
+            if (a[key] > b[key]) {
+              return 1;
+            }
+            if (a[key] < b[key]) {
+              return -1;
+            }
+            return 0;
+          });
+          sortOrder.value = !sortOrder.value;
+        } else {
+          filterPersons.value = filterPersons.value.sort((a, b) => {
+            if (a[key] > b[key]) {
+              return -1;
+            }
+            if (a[key] < b[key]) {
+              return 1;
+            }
+            return 0;
+          });
+          sortOrder.value = !sortOrder.value;
+        }
+      };
+      const filterByHeight = (key = '') => {
+        if (!sortOrder.value) {
+          filterPersons.value = filterPersons.value.sort((a, b) => {
+            if (parseInt(a[key]) > parseInt(b[key])) {
+              return 1;
+            }
+            if (parseInt(a[key]) < parseInt(b[key])) {
+              return -1;
+            }
+            return 0;
+          });
+          sortOrder.value = !sortOrder.value;
+        } else {
+          filterPersons.value = filterPersons.value.sort((a, b) => {
+            if (parseInt(a[key]) > parseInt(b[key])) {
+              return -1;
+            }
+            if (parseInt(a[key]) < parseInt(b[key])) {
+              return 1;
+            }
+            return 0;
+          });
+          sortOrder.value = !sortOrder.value;
+        }
+      };
+
+      watch(gender, (newGender) => {
+        if (newGender === 'all') {
+          filterPersons.value = persons.value;
+        }
+        if (newGender === 'm') {
+          filterPersons.value = persons.value.filter((person) => person.gender === 'male');
+        }
+        if (newGender === 'f') {
+          filterPersons.value = persons.value.filter((person) => person.gender === 'female');
+        }
+        if (newGender === 'other') {
+          filterPersons.value = persons.value.filter((person) => (person.gender !== 'female') && (person.gender !== 'male'));
+        }
+      });
+      return {
+        persons,
+        filterByName,
+        filterByHeight,
+        gender,
+        filterPersons,
+      }
     },
   };
 </script>
